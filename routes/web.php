f60713ff5743f359967a30e83f1c38eb8763d9e8
Sharkteeth89 <sharkteeth89spain@gmail.com>
2021-02-17 23:37:35 +0100

    Login finished

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', 'LoginController@index');
+Route::get('login/{provider}', 'LoginController@redirectToProvider');
+Route::get('{provider}/callback', 'LoginController@handleProviderCallback');
+Route::get('/home', function () {
+    return 'User is logged in';
+});
